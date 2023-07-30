910bf1d98dfef495090812ffa11f0724142eb46b
jplnb <jpabalanb@gmail.com>
2023-07-30 13:59:48 -0400

    add facts sheet link & css change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(async function(){
     // Fetch csv file
-    let gsheet_response = await fetch("https://docs.google.com/spreadsheets/d/17X0qAvHSblLjCMv6nugW8-He2MmOBQNjZnPD7XLWHu4/export?format=csv");
+    let gsheet_response = await fetch("https://docs.google.com/spreadsheets/d/1VVD8_KhWia3Zhcc6BHggQnn8gObYGCGG1gwuOk42pE4/export?format=csv");
     
     if(gsheet_response.ok) {
         // Parse csv
@@ -8,7 +8,7 @@ $(async function(){
         
         // Find number of weeks since start and take that mod the number of
         // facts, and add one to account for the first line.
-        let line = (dayjs().diff("2023-07-23","week") % (quote_csv.length-1)) + 1;
+        let line = (dayjs().diff("2023-07-30","week") % (quote_csv.length-1)) + 1;
         
         // Set fact
         $("#fact").text(quote_csv[line][1]);
